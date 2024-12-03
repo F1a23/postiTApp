a3e24bb95f4c86a50e1b12cece592361ad795fda
F1a23 <fatima5alamri8@gmail.com>
2024-12-03 08:38:49 +0400

    first commit

--- client/src/Store/store.js
+++ client/src/Store/store.js
@@ -1,10 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "../Features/UserSlice";
 import postReducer from "../Features/PostSlice";
+import { persistStore, persistReducer } from "redux-persist";
 
-export const store = configureStore({
-  reducer: {
-    users: usersReducer,
-    posts: postReducer,
-  },
+import storage from "redux-persist/lib/storage";
+
+import { combineReducers } from "redux";
+
+const persistConfig = {
+  key: "reduxstore",
+
+  storage,
+};
+const rootReducer = combineReducers({
+  users: usersReducer,
+
+  posts: postReducer,
 });
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+});
+const persistore = persistStore(store);
+export { store, persistore };
